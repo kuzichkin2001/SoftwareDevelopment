71c3efe51c7662e6bbd1aeaa8e598a33ba846e11
kuzichkin2001 <kuzichkin.p@yandex.ru>
2021-12-20 12:32:15 +0400

    Commit DI-container.

---
+++ DiContainer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.Unicode;
+using DiContainer.DependencyInjection;
+
+
+namespace DiContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new DiServiceCollection();
+
+            services.RegisterSingleton<IRandomGuidProvider, RandomGuidProvider>();
+            services.RegisterTransient<IService, FirstService>();
+
+            var container = services.GenerateContainer();
+
+            var singletonFirst = container.GetService<IRandomGuidProvider>();
+            var singletonSecond = container.GetService<IRandomGuidProvider>();
+
+            var transientFirst = container.GetService<IService>();
+            var transientSecond = container.GetService<IRandomGuidProvider>();
+
+            Console.WriteLine("Singletons:");
+            Console.WriteLine(singletonFirst.RandomGuid);
+            Console.WriteLine(singletonSecond.RandomGuid);
+
+            Console.WriteLine("\nTransients:");
+            transientFirst.PrintSomething();
+            Console.WriteLine(transientSecond.RandomGuid);
+        }
+    }
+}
